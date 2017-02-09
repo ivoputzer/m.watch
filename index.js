3c76565c03e88364503283a64bedc2074cabfcb2
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2017-02-09 19:39:49 +0100

    implements working draft

---
+++ index.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+const {watch} = require('fs')
+const {spawn} = require('child_process')
+
+const [cmd, ...args] = process.argv.slice(2)
+const notify = spawn.bind(null, cmd, args, {stdio: 'inherit'})
+
+watch(process.cwd(), {recursive: true, env: process.env}, notify)
